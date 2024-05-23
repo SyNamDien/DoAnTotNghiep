f2435a910899e0d46fbf7591433ad4e8c765ff06
SyNamDien <dkm170102@gmail.com>
2024-05-23 23:21:21 +0700

    BE

---
+++ FontEnd/web-giup-viec/src/views/ContactView.vue
@@ -0,0 +1,215 @@
+<template>
+    <body>
+        <header-user/>
+        <div class="slider-area ">
+            <div class="single-slider section-overly slider-height2 d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="hero-cap text-center">
+                                <h2>LIÊN HỆ</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="blog_area section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <h3 style="font-style: italic;">
+                            Quý khách hàng tìm hiểu về dịch vụ, đã sử dụng dịch vụ, đang sử dụng các dịch vụ của HomeCare cần liên hệ để được tư vấn, liên hệ để phản hồi chất lượng dịch vụ theo thông tin dưới đây:
+                        </h3>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <h2 style="font-weight: 700; font-size: 35px;">Công ty TNHH Dịch Vụ HomeCare</h2>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Tên thương hiệu: <span style="color: coral;">HomeCare</span></p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Email: GiupViecHomecare@gmail.com</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Điện thoại: 0868464826</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-3">
+                        <hr>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Để đặt hoặc tư vấn về dịch vụ có 02 cách:</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Cách 1: Email: GiupViecHomecare@gmail.com</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Cách 2:  Điện thoại: 0868464826</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <hr>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-3">
+                        <hr>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Để thanh toán có 02 cách:</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Cách 1: Trả trực tiếp cho tại công ty</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Cách 2:  Thanh toán qua ngân hàng</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Mã QR: 
+                            <img width="250"  src="assets/qrcode.png" />
+                        </p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p>Nội dung chuyển khoản : Mã đơn</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-3">
+                        <hr>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+
+                <div class="row mt-10">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p style="font-weight: bold;">ĐỊA CHỈ LIÊN HỆ:</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p style="font-weight: bold;">HÀ NỘI</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p style="font-weight: bold;">P2401 tầng 24 toà HH1, Số 90 Nguyễn Tuân, Thanh Xuân, Hà Nội</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p style="font-weight: bold;">TP HỒ CHÍ MINH</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p style="font-weight: bold;">04.14, tầng 4, Khu chung cư Garden Gate, 08 Hoàng Minh Giám, Phường 9, Quận Phú Nhuận, Thành phố Hồ Chí Minh</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p style="font-weight: bold;">HẢI PHÒNG</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p style="font-weight: bold;">Số 42 ngõ 333B Văn Cao, P Đằng Lâm, Q Hải An, TP Hải Phòng.</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+            </div>
+        </section>
+        <footer-user/>
+    </body>
+
+</template>
+<script>
+import FooterUser from '../components/FooterUser.vue'
+import HeaderUser from '../components/HeaderUser.vue'
+export default {
+    name: 'App',
+    components: { HeaderUser, FooterUser },
+}
+</script>
+<style>
+hr {
+    border: none; 
+    border-top: 2px solid black; 
+    margin: 20px 0;
+}
+</style>
